34567579815f574734d06638f2e238933c4a367f
USER <meljuncenita@gmail.com>
2025-02-03 17:35:39 +0800

    first day

---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <form action="#">
+        <div class="container">
+            <div class="welcome-container">
+                <label class="welcome">Welcome to Health Care</label>
+            </div>
+            
+            <div class="login-container">
+
+                <label class="login">Login Page</label>
+
+                <input type="text" id="username" name="login" placeholder="Username" required> <br>
+
+                <input type="text" id="password" name="login" placeholder="Password" required>
+
+                <input type="submit" value="SUBMIT">
+                
+                <a href="#"> Create Account</a>
+
+            </div>
+
+        </div>
+    </form>
+    
+</body>
+</html>
